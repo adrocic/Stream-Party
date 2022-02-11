7c4723ea8092c1fe66918cb1b80dcd854af43ca8
adrocic <anel.drocic@gmail.com>
2022-02-11 13:51:30 -0600

    fixed bug where link was returning undefined, fixed bug where links where not active, removed extra intellijIdea files

--- StreamParty/stream-party.js
+++ StreamParty/stream-party.js
@@ -1,32 +1,27 @@
-import { Client } from 'discord.js';
-import { makeInviteRequest } from './api.js';
-
+import { Client } from "discord.js";
+import { makeInviteRequest } from "./api.js";
 
 class StreamParty {
-
   constructor(client, defaultApps) {
-    if (!client) throw new Error('No Client Present...');
+    if (!client) throw new Error("No Client Present...");
 
     this.client = client;
 
     this.apps = { ...defaultApps };
-    console.log(this.apps)
+    console.log(this.apps);
   }
 
-  async createStreamParty(voiceChannelId, option) {
-    const returnData = {
-      code: 'none',
-    };
-    if (option && this.apps[option.toLowerCase()]) {
-      const appId = this.apps[option.toLowerCase()];
-      try {
-        inviteCode = makeInviteRequest(appId, voiceChannelId, this.client.token);
-        return inviteCode;
-      } catch (err) {
-        throw new Error('An error has occurred while starting Youtube Together...');
-      }
-    } else {
-      throw new Error('Invalid option...');
+ createStreamParty = async (voiceChannelId, option = "") => {
+    if (!option.toLocaleLowerCase() in this.apps) throw new Error(err);
+    const appId = this.apps[option.toLowerCase()];
+    try {
+      return await makeInviteRequest(
+        appId,
+        voiceChannelId,
+        this.client.token
+      );
+    } catch (err) {
+      throw new Error(err);
     }
   }
 }
--- bot.js
+++ bot.js
@@ -1,28 +1,30 @@
-import dotenv from 'dotenv';
-import Discord from 'discord.js';
-import StreamParty  from './StreamParty/stream-party.js';
-import { defaultApps } from './StreamParty/defaultApps.js';
+import dotenv from "dotenv";
+import Discord from "discord.js";
+import StreamParty from "./StreamParty/stream-party.js";
+import { defaultApps } from "./StreamParty/defaultApps.js";
 
+process.env.NODE_ENV !== "production" && dotenv.config();
 
-process.env.NODE_ENV !== 'production' && dotenv.config();
-
-const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Discord.Client({
+  intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES],
+});
 
 client.streamParty = new StreamParty(client, defaultApps);
 
-client.on('interactionCreate', async interaction => {
-    if(!interaction.isCommand()) return
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
 
-    const channel = interaction.channel;
+  const channel = interaction.channel;
 
-    if (interaction.commandName === 'party') {
-        if(interaction.channelId) {
-            client.streamParty.createStreamParty(interaction.channelId, 'youtube').then(async invite => {
-                console.log(invite);
-                return channel.send(`${invite.code}`);
-            });
-        };
-    };
+  if (interaction.commandName === "party") {
+    if (channel) {
+      client.streamParty
+        .createStreamParty(interaction.member.voice.channelId, "youtube")
+        .then(async (invite) => {
+          return channel.send(`${invite}`);
+        });
+    }
+  }
 });
 
 client.login(`${process.env.AUTH_TOKEN}`);
