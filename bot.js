bf437fecf61427cd8f4f65dbb647d8ec613654e4
adrocic <anel.drocic@gmail.com>
2022-02-11 03:38:16 -0600

    updated bot to use interaction slash command instead of message

--- bot.js
+++ bot.js
@@ -1,18 +1,20 @@
-const Discord = require('discord.js');
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
+const Discord = require('discord.js');
 
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
 const { DiscordTogether } = require('discord-together');
 
 client.discordTogether = new DiscordTogether(client);
 
-client.on('messageCreate', async message => {
-    if (message.content === 'party') {
-        if(message.member.voice.channel) {
-            client.discordTogether.createTogetherCode(message.member.voice.channel.id, 'youtube').then(async invite => {
-                return message.channel.send(`${invite.code}`);
+client.on('interactionCreate', async interaction => {
+    if(!interaction.isCommand()) return
+
+    if (interaction.commandName === 'party') {
+        if(interaction.member.voice.channel) {
+            client.discordTogether.createTogetherCode(interaction.member.voice.channel.id, 'youtube').then(async invite => {
+                return interaction.channel.send(`${invite.code}`);
             });
         };
     };
